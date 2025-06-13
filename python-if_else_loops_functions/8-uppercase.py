f11d9c33003868703d01cd83f68adf8aa4e1ed4e
HuseynEyvazov0 <heyvazov22@gmail.com>
2025-06-13 17:23:06 +0000

    salam

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -4,7 +4,7 @@ c = "Best School 98 Battery street"
 def uppercase(str):
     result = ""
     for c in str:
-        if 97 <= ord(c) >= 122:
+        if 97 <= ord(c) <= 122:
             result += chr(ord(c) - 32)
         else:
             result += c
