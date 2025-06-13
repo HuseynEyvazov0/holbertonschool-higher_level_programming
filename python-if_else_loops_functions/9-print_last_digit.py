d4592dcebb9585033612ecbf23262996d4b37e80
HuseynEyvazov0 <heyvazov22@gmail.com>
2025-06-13 17:36:57 +0000

    salam

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+
 def print_last_digit(number):
+    if not isinstance(number, int):
+        raise TypeError("Input must be an integer")
+
     last_digit = abs(number) % 10
-    print(last_digit)
+    print(last_digit, end="")
     return last_digit
