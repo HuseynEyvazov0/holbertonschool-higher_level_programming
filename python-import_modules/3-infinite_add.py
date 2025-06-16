5c06815fa577b1f0f185f86ca221c97782d880b8
HuseynEyvazov0 <heyvazov22@gmail.com>
2025-06-16 14:56:58 +0000

    salanm#

---
+++ python-import_modules/3-infinite_add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    argv = sys.argv
+    argc = len(argv)
+    arg = 0
+
+    for i in range(1,argc):
+        arg +=int(argv[i])
+print(arg)
